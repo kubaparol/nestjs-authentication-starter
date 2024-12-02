d42c6130f9bbe2f834092089d9f7740039f11ac7
kubaparol <kubciop33@gmail.com>
2024-12-02 11:10:08 +0100

    ft: implement JWT guard and public route decorator for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard } from './auth/guards/jwt.guard';
 
 @Module({
   imports: [
@@ -10,6 +12,11 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     UsersModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtGuard } from './guards/jwt.guard';
 import { LocalGuard } from './guards/local.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalGuard)
   @Post('login')
   async login(@Request() req) {
