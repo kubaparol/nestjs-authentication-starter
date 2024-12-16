8120576e5e8f7f50d95e2d7c95d026157d17378f
kubaparol <kubciop33@gmail.com>
2024-12-16 16:45:12 +0100

    ft: add swagger documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(3000);
+  const config = new DocumentBuilder() //
+    .setTitle('NestJS Authentication Starter')
+    .setDescription('REST API for NestJS Authentication Starter')
+    .setVersion('0.0.1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
