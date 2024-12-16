d57f4225915a5f8c3164f45ba600bad1033102ad
kubaparol <kubciop33@gmail.com>
2024-12-16 17:28:41 +0100

    ft: enhance Swagger documentation for authentication endpoints and add ApiProperty to SignUpDto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,8 @@ import { LocalGuard } from './guards';
 import { Public } from './decorators';
 import { SignUpDto, SignInDto } from './dtos';
 import { AccessTokenResponse } from './responses';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
+import { Swagger } from '@decorators';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -12,18 +13,22 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
-  @ApiOperation({
-    summary: 'Sign in',
-    description: 'Sign in to the application',
+  @Swagger({
+    operation: {
+      summary: 'Sign in',
+      description: 'Sign in to the application',
+    },
+    responses: [
+      {
+        status: 201,
+        description: 'Sign in successful',
+        type: AccessTokenResponse,
+      },
+      { status: 404, description: 'User not found' },
+      { status: 401, description: 'Invalid password' },
+      { status: 400, description: 'Bad request' },
+    ],
   })
-  @ApiResponse({
-    status: 201,
-    description: 'Sign in successful',
-    type: AccessTokenResponse,
-  })
-  @ApiResponse({ status: 404, description: 'User not found' })
-  @ApiResponse({ status: 401, description: 'Invalid password' })
-  @ApiResponse({ status: 400, description: 'Bad request' })
   @UseGuards(LocalGuard)
   @Post('signin')
   async signin(@Body() dto: SignInDto): Promise<AccessTokenResponse> {
@@ -31,6 +36,20 @@ export class AuthController {
   }
 
   @Public()
+  @Swagger({
+    operation: {
+      summary: 'Sign up',
+      description: 'Sign up for the application',
+    },
+    responses: [
+      {
+        status: 201,
+        description: 'Sign up successful',
+        type: AccessTokenResponse,
+      },
+      { status: 400, description: 'Bad request' },
+    ],
+  })
   @Post('signup')
   async signup(@Body() dto: SignUpDto): Promise<AccessTokenResponse> {
     return this.authService.signUp(dto);
--- src/auth/dtos/sign-up.dto.ts
+++ src/auth/dtos/sign-up.dto.ts
@@ -1,18 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignUpDto {
+  @ApiProperty({
+    example: 'John',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty({
+    example: 'Doe',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty({
+    example: 'johndoe@example.com',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: '123456789',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
