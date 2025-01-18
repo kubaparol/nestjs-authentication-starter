37fe03c5ff8a8d03ab5892e1fb3aa89956db96d5
kubaparol <kubciop33@gmail.com>
2025-01-18 12:59:11 +0100

    ft: add mailer module with handlebars support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,40 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard } from './auth/guards';
 import { PrismaModule } from './prisma/prisma.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        defaults: {
+          from: configService.get<string>('EMAIL_FROM'),
+        },
+        transport: {
+          host: configService.get<string>('EMAIL_HOST'),
+          port: configService.get<number>('EMAIL_PORT'),
+          secure: configService.get<boolean>('EMAIL_SECURE'),
+          auth: {
+            user: configService.get<string>('EMAIL_USER'),
+            pass: configService.get<string>('EMAIL_PASSWORD'),
+          },
+        },
+        template: {
+          dir: __dirname + './../commons/email-templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
     UsersModule,
     PrismaModule,
